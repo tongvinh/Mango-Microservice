23d8bc0b76223abe5aff7a9bb171196a1c4df848
Vinh Tong <vinhtong.dev@gmail.com>
2023-06-28 15:36:58 +0700

    2.7 Response Common

--- Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
+++ Mango.Services.CouponAPI/Controllers/CouponAPIController.cs
@@ -1,5 +1,6 @@
 using Mango.Services.CouponAPI.Data;
 using Mango.Services.CouponAPI.Models;
+using Mango.Services.CouponAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Mango.Services.CouponAPI.Controllers;
@@ -9,44 +10,46 @@ namespace Mango.Services.CouponAPI.Controllers;
 public class CouponAPIController : ControllerBase
 {
     private readonly AppDbContext _db;
-
+    private ResponseDto _response;
+    
     public CouponAPIController(AppDbContext db)
     {
         _db = db;
+        _response = new ResponseDto();
     }
 
     [HttpGet]
-    public object Get()
+    public ResponseDto Get()
     {
         try
         {
             IEnumerable<Coupon> objList = _db.Coupons.ToList();
-            return objList;
+            _response.Result = objList;
         }
         catch (Exception ex)
         {
-            Console.WriteLine(ex);
-            throw;
+            _response.IsSuccess = false;
+            _response.Message = ex.Message;
         }
 
-        return null;
+        return _response;
     }
 
     [HttpGet]
     [Route("{id:int}")]
-    public object Get(int id)
+    public ResponseDto Get(int id)
     {
         try
         {
             Coupon obj = _db.Coupons.First(x => x.CouponId == id);
-            return obj;
+            _response.Result = obj;
         }
         catch (Exception ex)
         {
-            Console.WriteLine(ex);
-            throw;
+            _response.IsSuccess = false;
+            _response.Message = ex.Message;
         }
 
-        return null;
+        return _response;
     }
 }
