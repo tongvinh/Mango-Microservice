de58ed5dd02ad9e2f0674b5801879b491dd7a34c
Vinh Tong <vinhtong.dev@gmail.com>
2023-07-06 14:27:23 +0700

    5.5 Service Token Provider

--- Mango.Web/Program.cs
+++ Mango.Web/Program.cs
@@ -17,6 +17,7 @@ builder.Services.AddControllersWithViews();
 });*/
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddHttpClient();
+builder.Services.AddScoped<ITokenProvider, TokenProvider>();
 builder.Services.AddHttpClient<ICouponService, CouponService>();
 builder.Services.AddHttpClient<IAuthService, AuthService>();
 SD.CouponAPIBase = builder.Configuration["ServiceUrls:CouponAPI"];
