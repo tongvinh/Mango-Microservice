46edd2a87ed3cfe5fdfa713e3047c78d02895c38
Vinh Tong <vinhtong.dev@gmail.com>
2023-07-07 16:44:15 +0700

    5.12 Passing Token to API

--- Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
+++ Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ab56d221d3f3edeb2ca3c58759dbce86387cec7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e914fb1830b1e12fcbb6730f9a93c2c370d21595")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
