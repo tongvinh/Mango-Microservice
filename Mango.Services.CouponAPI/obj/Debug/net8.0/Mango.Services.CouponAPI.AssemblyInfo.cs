e914fb1830b1e12fcbb6730f9a93c2c370d21595
Vinh Tong <vinhtong.dev@gmail.com>
2023-07-07 16:39:16 +0700

    5.11 Add Bearer into Swagger Gen

--- Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
+++ Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c1690109d1cdc7dc727a11c48d9d6b62e4cbb25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7e7367d8865aaed94fdbe45bcefcd0faa622b758")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
