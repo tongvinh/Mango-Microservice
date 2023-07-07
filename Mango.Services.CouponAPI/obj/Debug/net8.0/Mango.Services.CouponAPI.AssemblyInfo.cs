7e7367d8865aaed94fdbe45bcefcd0faa622b758
Vinh Tong <vinhtong.dev@gmail.com>
2023-07-07 14:44:19 +0700

    5.10 Internal Error

--- Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
+++ Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7c28d542d3ef49d89805e10aee003dc5e1a97d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c1690109d1cdc7dc727a11c48d9d6b62e4cbb25")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
