c409572f0c029fde61cc9c96e7d77204ad67f2a2
Vinh Tong <vinhtong.dev@gmail.com>
2023-07-10 16:30:07 +0700

    7.1 UI Home Page

--- Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
+++ Mango.Web/obj/Debug/net8.0/Mango.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a777f889f6e8b38145e8b9c8336d7d5d89e075f9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c617abb9b0cdcd25ee5839211ba223154c10d8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
