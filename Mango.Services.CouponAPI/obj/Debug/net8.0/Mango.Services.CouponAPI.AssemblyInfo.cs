9cbf732b93cc200f379f9d67ca7a6fca1150bd5d
Vinh Tong <vinhtong.dev@gmail.com>
2023-07-12 11:10:29 +0700

    9.2 add Product into Cart

--- Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
+++ Mango.Services.CouponAPI/obj/Debug/net8.0/Mango.Services.CouponAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53639aee892bdecad1b8aacc6bb9a1bccc75f08d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mango.Services.CouponAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Mango.Web/Controllers/HomeController.cs
+++ Mango.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 using System.Diagnostics;
+using IdentityModel;
 using Microsoft.AspNetCore.Mvc;
 using Mango.Web.Models;
 using Mango.Web.Service.IService;
@@ -11,10 +12,12 @@ namespace Mango.Web.Controllers;
 public class HomeController : Controller
 {
     private readonly IProductService _productService;
+    private readonly ICartService _cartService;
 
-    public HomeController(IProductService productService)
+    public HomeController(IProductService productService, ICartService cartService)
     {
         _productService = productService;
+        _cartService = cartService;
     }
 
     public async Task<IActionResult> Index()
@@ -51,6 +54,42 @@ public class HomeController : Controller
         }
 
         return View(model);
+    }
+
+    [Authorize]
+    [HttpPost]
+    [ActionName("ProductDetails")]
+    public async Task<IActionResult> ProductDetails(ProductDto productDto)
+    {
+        CartDto cartDto = new CartDto()
+        {
+            CartHeader = new CartHeaderDto()
+            {
+                UserId = User.Claims.Where(u => u.Type == JwtClaimTypes.Subject)?.FirstOrDefault()?.Value,
+            }
+        };
+        CartDetailsDto cartDetails = new CartDetailsDto()
+        {
+            Count = productDto.Count,
+            ProductId = productDto.ProductId
+        };
+        
+        List<CartDetailsDto> cartDetailsDtos = new List<CartDetailsDto>(){ cartDetails };
+        cartDto.CartDetails = cartDetailsDtos;
+
+        ResponseDto? response = await _cartService.UpsertCartAsync(cartDto);
+
+        if (response != null && response.IsSuccess)
+        {
+            TempData["success"] = "Item has been added to the Shopping Cart";
+            return RedirectToAction(nameof(Index));
+        }
+        else
+        {
+            TempData["error"] = response?.Message;
+        }
+
+        return View(productDto);
     }
 
     [Authorize(Roles = SD.RoleAdmin)]
