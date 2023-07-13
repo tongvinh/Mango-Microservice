2a693c5eb127094ae91fc401957e1b58ca29f968
Vinh Tong <vinhtong.dev@gmail.com>
2023-07-13 16:01:07 +0700

    11.2 Register Methods to Processor

--- Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
+++ Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
@@ -1,8 +1,9 @@
+using System.Text;
 using Azure.Messaging.ServiceBus;
 
 namespace Mango.Services.EmailAPI.Messaging;
 
-public class AzureServiceBusConsumer
+public class AzureServiceBusConsumer:IAzureServiceBusConsumer
 {
     private readonly string? serviceBusConnectioString;
     private readonly string emailCartQueue;
@@ -18,5 +19,40 @@ public class AzureServiceBusConsumer
 
         var client = new ServiceBusClient(serviceBusConnectioString);
         _emailCartProcessor = client.CreateProcessor(emailCartQueue);
+    }
+
+    public async Task Start()
+    {
+        _emailCartProcessor.ProcessMessageAsync += OnEmailCartRequestReceived;
+        _emailCartProcessor.ProcessErrorAsync += ErrorHandler;
+    }
+    
+    public async Task Stop()
+    {
+        await _emailCartProcessor.StopProcessingAsync();
+        await _emailCartProcessor.DisposeAsync();
+    }
+
+    private Task ErrorHandler(ProcessErrorEventArgs arg)
+    {
+        Console.WriteLine(arg.Exception.ToString());
+        return Task.CompletedTask;
+    }
+
+    private async Task OnEmailCartRequestReceived(ProcessMessageEventArgs arg)
+    {
+        // This is where you will receive message
+        var message = arg.Message;
+        var body = Encoding.UTF8.GetString(message.Body);
+        try
+        {
+            //TODO - Try to log email
+            await arg.CompleteMessageAsync(arg.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            throw;
+        }
     }
 }
---
+++ Mango.Services.EmailAPI/Messaging/IAzureServiceBusConsumer.cs
@@ -0,0 +1,7 @@
+namespace Mango.Services.EmailAPI.Messaging;
+
+public interface IAzureServiceBusConsumer
+{
+    Task Start();
+    Task Stop();
+}
