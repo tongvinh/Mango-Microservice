61bde7be5451f6ac1a8aa91c4aee0a98e1d82902
Vinh Tong <vinhtong.dev@gmail.com>
2023-07-14 11:03:30 +0700

    11.7 Assignment Solution Part2

--- Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
+++ Mango.Services.EmailAPI/Messaging/AzureServiceBusConsumer.cs
@@ -10,10 +10,12 @@ public class AzureServiceBusConsumer:IAzureServiceBusConsumer
 {
     private readonly string? serviceBusConnectioString;
     private readonly string emailCartQueue;
+    private readonly string registerUserQueue;    
     private readonly IConfiguration _configuration;
     private readonly EmailService _emailService;
 
     private ServiceBusProcessor _emailCartProcessor;
+    private ServiceBusProcessor _registerUserProcessor;
     
     public AzureServiceBusConsumer(IConfiguration configuration,EmailService emailService)
     {
@@ -21,9 +23,11 @@ public class AzureServiceBusConsumer:IAzureServiceBusConsumer
         _emailService = emailService;
         serviceBusConnectioString = _configuration.GetValue<string>("ServiceBusConnectionString");
         emailCartQueue = _configuration.GetValue<string>("TopicAndQueueNames:EmailShoppingCartQueue");
+        registerUserQueue = _configuration.GetValue<string>("TopicAndQueueNames:RegisterUserQueue");
 
         var client = new ServiceBusClient(serviceBusConnectioString);
         _emailCartProcessor = client.CreateProcessor(emailCartQueue);
+        _registerUserProcessor = client.CreateProcessor(registerUserQueue);
     }
 
     public async Task Start()
@@ -31,13 +35,20 @@ public class AzureServiceBusConsumer:IAzureServiceBusConsumer
         _emailCartProcessor.ProcessMessageAsync += OnEmailCartRequestReceived;
         _emailCartProcessor.ProcessErrorAsync += ErrorHandler;
         await _emailCartProcessor.StartProcessingAsync();
+
+        _registerUserProcessor.ProcessMessageAsync += OnUserRegisterRequestReceived;
+        _registerUserProcessor.ProcessErrorAsync += ErrorHandler;
+        await _registerUserProcessor.StartProcessingAsync();
     }
-    
+
     public async Task Stop()
     {
         await _emailCartProcessor.StopProcessingAsync();
         await _emailCartProcessor.DisposeAsync();
-        await _emailCartProcessor.StopProcessingAsync();
+
+        await _registerUserProcessor.StopProcessingAsync();
+        await _registerUserProcessor.DisposeAsync();
+
     }
 
     private Task ErrorHandler(ProcessErrorEventArgs arg)
@@ -57,6 +68,24 @@ public class AzureServiceBusConsumer:IAzureServiceBusConsumer
         {
             //TODO - Try to log email
             await _emailService.EmailCartAndLog(objMessage);
+            await arg.CompleteMessageAsync(arg.Message);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+            throw;
+        }
+    }
+    
+    private async Task OnUserRegisterRequestReceived(ProcessMessageEventArgs arg)
+    {
+        var message = arg.Message;
+        var body = Encoding.UTF8.GetString(message.Body);
+
+        string email = JsonConvert.DeserializeObject<string>(body);
+        try
+        {
+            await _emailService.RegisterUserEmailAndLog(email);
             await arg.CompleteMessageAsync(arg.Message);
         }
         catch (Exception ex)
--- Mango.Services.EmailAPI/Services/EmailService.cs
+++ Mango.Services.EmailAPI/Services/EmailService.cs
@@ -33,6 +33,12 @@ public class EmailService:IEmailService
         await LogAndEmail(message.ToString(), cartDto.CartHeader.Email);
     }
 
+    public async Task RegisterUserEmailAndLog(string email)
+    {
+        string message = "User Registeration Successful. <br/> Email :" + email;
+        await LogAndEmail(message, "dotnetmastery@gmail.com");
+    }
+
     private async Task<bool> LogAndEmail(string message, string email)
     {
         try
