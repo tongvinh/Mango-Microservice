aa68230d3d60044bda895096a5fb66c8fdabef5b
Vinh Tong <vinhtong.dev@gmail.com>
2023-07-14 16:48:34 +0700

    12.6 MappingConfig OrderAPI

--- Mango.Services.OrderAPI/MappingConfig.cs
+++ Mango.Services.OrderAPI/MappingConfig.cs
@@ -1,4 +1,6 @@
 using AutoMapper;
+using Mango.Services.OrderAPI.Models;
+using Mango.Services.OrderAPI.Models.Dto;
 
 namespace Mango.Services.OrderAPI;
 
@@ -8,7 +10,15 @@ public class MappingConfig
     {
         var mappingConfig = new MapperConfiguration(config =>
         {
+            config.CreateMap<OrderHeaderDto, CartHeaderDto>()
+                .ForMember(dest => dest.CartTotal, u => u.MapFrom(src => src.OrderTotal)).ReverseMap();
+            config.CreateMap<CartDetailsDto, OrderDetailDto>()
+                .ForMember(dest => dest.ProductName, u => u.MapFrom(src => src.Product.Price));
+
+            config.CreateMap<OrderDetailDto, CartDetailsDto>();
             
+            config.CreateMap<OrderHeader, OrderHeaderDto>().ReverseMap();
+            config.CreateMap<OrderDetailDto, OrderDetails>().ReverseMap();
         });
         return mappingConfig;
     }
